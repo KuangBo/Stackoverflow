b51b09d052ba45dc7dc5c457b1d88471cd7f6023
KuangBo <193976337@qq.com>
2019-04-03 10:15:28 +0800

    Stack Overflow

---
+++ githubPro/main.py
@@ -0,0 +1,6 @@
+# -*- coding=utf8 -*-
+from scrapy import cmdline
+
+
+# cmdline.execute("scrapy crawl stackoverflow -L WARNING".split())  # 不打印Debug信息
+cmdline.execute("scrapy crawl stackoverflow".split())

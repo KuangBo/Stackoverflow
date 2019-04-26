5663c71fb3ebde8680f2d1a774c5c8a68d11528d
KuangBo <193976337@qq.com>
2019-04-26 09:54:38 +0800

    在项目根目录加入__init__.py文件

--- traverse/main.py
+++ traverse/main.py
@@ -9,9 +9,9 @@ def all_path(dirname):
     result = []  # 所有的文件
 
     for maindir, subdir, file_name_list in os.walk(dirname):
-        # print("1:",maindir) #当前主目录
-        # print("2:",subdir) #当前主目录下的所有目录
-        # print("3:",file_name_list)  #当前主目录下的所有文件
+        # print("1:",maindir)  # 当前主目录
+        # print("2:",subdir)  # 当前主目录下的所有目录
+        # print("3:",file_name_list)  # 当前主目录下的所有文件
         for filename in file_name_list:
             apath = os.path.join(maindir, filename)  # 合并成一个完整路径
             ext = os.path.splitext(apath)[1]  # 获取文件后缀 [0]获取的是除了文件名以外的内容
